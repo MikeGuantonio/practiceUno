e2b993a1d4d62584d7cf7d5a8391afb5d3003dad
Mike Guantonio <mike.guantonio@gmail.com>
2013-03-13 00:22:30 -0400

    UNO: Abstracted the card objects. May move behavior outside and into an interface.

--- src/practiceUno/Deck.java
+++ src/practiceUno/Deck.java
@@ -20,7 +20,8 @@ public class Deck
      */
     public Deck()
     {   
-        CreateDeck(); 
+       CreateNumbers(); 
+       CreateSpecial(); 
     }
     
     private void AddToDeck(Card c, int numberOfCards)
@@ -29,45 +30,26 @@ public class Deck
             deck.push(c);
     }
      
-    private void CreateDeck()
+    private void CreateNumbers()
     {
-	Card tmpCard; 
-
-	for(Card.cardColor color : Card.cardColor.values())
-	{
-	    for(Card.cardFace value : Card.cardFace.values())
-	    {
-		if(color != Card.cardColor.BLANK)
-		{
-		    if(value != Card.cardFace.BLANK)
-		    {
-			if(value == Card.cardFace.ZERO)
-                            AddToDeck(new Card(color, value, ""), 1);
-			else
-                            AddToDeck(new Card(color, value, ""), 2);
-		    }
-		}
-	    }
-	
-
-	    for(Card.cardSpecial special : Card.cardSpecial.values())
-	    {
-		if(special != Card.cardSpecial.BLANK)
-			AddToDeck(new Card(color, special, ""), 2);
-	    }
-	}
-        
-	for(Card.cardWild sp : Card.cardWild.values())
+        for(Card.cardColor color : Card.cardColor.values())
         {
-		    if(sp != Card.cardWild.BLANK)
-                        AddToDeck(new Card(sp, ""), 4);	
-        }
-        if(deck.size()== MAX_CARD)
-            System.out.println("Deck is correct");
-        else
-            System.out.println("Size: " + deck.size());
-	 
+            AddToDeck( new NumberCard (0, color), 1);
+            for (int i = 1; i < 10; i++) 
+                AddToDeck( new NumberCard (i, color), 2);
+        }      
     }
+    
+    private void CreateSpecial()
+    {
+        for(SpecialCard.cardValues sp : SpecialCard.cardValues.values())
+        {
+            for(Card.cardColor color : Card.cardColor.values())
+            {
+                AddToDeck(new SpecialCard(sp, color), 2);
+            }
+        }
+    }    
 
     /**
      *
@@ -75,16 +57,16 @@ public class Deck
      */
     public Card DrawNext()
     {
-        Card c = new Card(Card.cardColor.BLANK, Card.cardFace.BLANK, "" ); 
         if(!deck.isEmpty())
         {
-            c = (Card)deck.pop();
+            return (Card)deck.pop();
         }
         else
         {
             System.out.println("This deck is done. Need to shuffle and replace deck.");
         }
-	return c; 
+        return null;
+
     }
     
     /**
