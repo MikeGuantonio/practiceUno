58ad3c4637700d57dac8d480d1f893f82a7ef4b4
Mike Guantonio <mike.guantonio@gmail.com>
2013-03-27 13:25:52 -0400

    UNO: Added some notes for the test files.'

--- testScripts/test.sh
+++ testScripts/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Test the return value from java -jar and if it is bad do something. Else append output. 
+#Append each term run to output file for easy running. 
+#Find a way to return only the exceptions as a report. 
 for i in {1..10};
 do
     java -jar ../dist/UnoClone.jar 2>testResult.txt;
